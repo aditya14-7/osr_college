c4adbb2711b27d10f8b117aa71bf123a8c91bf54
Aditya Pandey <adityapnd14@gmail.com>
2022-07-04 15:04:07 +0530

    updated about page

--- about.php
+++ about.php
@@ -34,7 +34,221 @@
 
         <!-- body will go here (start) -->
 
+        <!-- breadcrumb (start) -->
+        <div class="edu-breadcrumb-area breadcrumb-style-1 ptb--60 ptb_md--40 ptb_sm--40 bg-image">
+            <div class="container eduvibe-animated-shape">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="breadcrumb-inner text-start">
+                            <div class="page-title">
+                                <h3 class="title">About Us-1</h3>
+                            </div>
+                            <nav class="edu-breadcrumb-nav">
+                                <ol class="edu-breadcrumb d-flex justify-content-start liststyle">
+                                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                    <li class="separator"><i class="ri-arrow-drop-right-line"></i></li>
+                                    <li class="breadcrumb-item active" aria-current="page">About Us</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
 
+                <!-- <div class="shape-dot-wrapper shape-wrapper d-xl-block d-none">
+                    <div class="shape-dot-wrapper shape-wrapper d-xl-block d-none">
+                        <div class="shape-image shape-image-1">
+                            <img src="assets/images/shapes/shape-11-07.png" alt="Shape Thumb" />
+                        </div>
+                        <div class="shape-image shape-image-2">
+                            <img src="assets/images/shapes/shape-01-02.png" alt="Shape Thumb" />
+                        </div>
+                        <div class="shape-image shape-image-3">
+                            <img src="assets/images/shapes/shape-03.png" alt="Shape Thumb" />
+                        </div>
+                        <div class="shape-image shape-image-4">
+                            <img src="assets/images/shapes/shape-13-12.png" alt="Shape Thumb" />
+                        </div>
+                        <div class="shape-image shape-image-5">
+                            <img src="assets/images/shapes/shape-36.png" alt="Shape Thumb" />
+                        </div>
+                        <div class="shape-image shape-image-6">
+                            <img src="assets/images/shapes/shape-05-07.png" alt="Shape Thumb" />
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+        <!-- breadcrumb (end) -->
+
+        <!-- about - who we are (start) -->
+        <div class="home-three-about edu-about-area about-style-4 bg-color-white edu-section-gapBottom">
+            <div class="container eduvibe-animated-shape">
+                <div class="row g-lg-5 g-md-5 g-sm-5">
+                    <div class="col-lg-12 col-xl-6">
+                        <div class="gallery-wrapper">
+                            <img class="image-1" src="assets/images/other_images/school-2.jpg" alt="About Images">
+                            <!-- <img class="image-2" src="" alt="About Images"> -->
+                            <!-- <img class="image-3" data-parallax='{"x": 0, "y": -120}' src="assets/images/about/about-05/about-group-03.jpg" alt="About Images"> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-12 col-xl-6">
+                        <div class="inner mt_mobile--40">
+                            <div class="section-title text-start" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                                <span class="pre-title">Who We Are</span>
+                                <h3 class="title">About College</h3>
+                            </div>
+                            <div class="feature-style-6 mt--40">
+                                <div class="edu-feature-list color-variation-1" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+
+                                    <div class="content">
+                                        <h6 class="title">We Offer The Best Carrier</h6>
+                                        <p class="description">
+                                            O.S.R. Group of Colleges Lucknow is located on Mahmudabad Road, 20 km from state capital Lucknow. m., 25 km from district headquarter Barabaki and 30 km from Mahmudabad. m. The important and prestigious town of Barabanki district is situated in Kursi. The college is affiliated to Dr. Ram Manohar Lohia Avadh University. This college has been established for the educational and social development of this area. The college is well equipped with the latest equipment/resources and laboratory. Teaching facility for all subjects of Arts and Science stream is available in the college. Wi-Fi-equipped facilities: There is a well-equipped computer lab and a rich library of art, religion, culture as well as all the subjects being run.
+                                        </p>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+        <!-- about - who we are (end) -->
+
+
+        <!-- Gallery (start) -->
+        <div class="eduvibe-home-three-event edu-event-area bg-color-white edu-section-gap">
+            <div class="container eduvibe-animated-shape">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section-title text-center" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                            <span class="pre-title">Our gallery</span>
+                            <h3 class="title">Some picturs to tell our story</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row g-5 mt--20">
+                      <div class="owl-carousel gallery-carousel owl-theme">
+                        <div class="item">
+                          <!-- Start Event Grid   -->
+                          <div class="col-lg-12 col-12" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                              <div class="edu-event event-grid-1 bg-shade radius-small">
+                                  <div class="inner">
+                                      <div class="thumbnail">
+                                          <a href="assets/images/other_images/school-2.jpg" class="fancybox" data-fancybox="gallery">
+                                                <img src="assets/images/other_images/school-2.jpg" style="height:250px;" alt="Event Images">
+                                          </a>
+                                          <div class="top-position status-group left-top">
+                                              <span class="eduvibe-status status-06">04 Dec 2022</span>
+                                          </div>
+                                      </div>
+                                      <div class="content">
+                                          <ul class="event-meta">
+                                              <li><i class="icon-arrow-right-line-right"></i>About Picture</li>
+                                          </ul>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <!-- End Event Grid   -->
+                        </div>
+
+                        <div class="item">
+                          <!-- Start Event Grid   -->
+                          <div class="col-lg-12 col-12" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                              <div class="edu-event event-grid-1 bg-shade radius-small">
+                                  <div class="inner">
+                                      <div class="thumbnail">
+                                          <a href="assets/images/other_images/school-2.jpg" class="fancybox" data-fancybox="gallery">
+                                              <img src="assets/images/other_images/school-2.jpg" style="height:250px;" alt="Event Images">
+                                          </a>
+                                          <div class="top-position status-group left-top">
+                                              <span class="eduvibe-status status-06">04 Dec 2022</span>
+                                          </div>
+                                      </div>
+                                      <div class="content">
+                                          <ul class="event-meta">
+                                              <li><i class="icon-arrow-right-line-right"></i>About Picture</li>
+                                          </ul>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <!-- End Event Grid   -->
+                        </div>
+
+                        <div class="item">
+                          <!-- Start Event Grid   -->
+                          <div class="col-lg-12 col-12" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                              <div class="edu-event event-grid-1 bg-shade radius-small">
+                                  <div class="inner">
+                                      <div class="thumbnail">
+                                          <a href="assets/images/other_images/school-2.jpg" class="fancybox" data-fancybox="gallery">
+                                              <img src="assets/images/other_images/school-2.jpg" style="height:250px;" alt="Event Images">
+                                          </a>
+                                          <div class="top-position status-group left-top">
+                                              <span class="eduvibe-status status-06">04 Dec 2022</span>
+                                          </div>
+                                      </div>
+                                      <div class="content">
+                                          <ul class="event-meta">
+                                              <li><i class="icon-arrow-right-line-right"></i>About Picture</li>
+                                          </ul>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <!-- End Event Grid   -->
+                        </div>
+
+                        <div class="item">
+                          <!-- Start Event Grid   -->
+                          <div class="col-lg-12 col-12" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                              <div class="edu-event event-grid-1 bg-shade radius-small">
+                                  <div class="inner">
+                                      <div class="thumbnail">
+                                          <a href="assets/images/other_images/school-2.jpg" class="fancybox" data-fancybox="gallery">
+                                              <img src="assets/images/other_images/school-2.jpg" style="height:250px;" alt="Event Images">
+                                          </a>
+                                          <div class="top-position status-group left-top">
+                                              <span class="eduvibe-status status-06">04 Dec 2022</span>
+                                          </div>
+                                      </div>
+                                      <div class="content">
+                                          <ul class="event-meta">
+                                              <li><i class="icon-arrow-right-line-right"></i>About Picture</li>
+                                          </ul>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <!-- End Event Grid   -->
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- <div class="shape-dot-wrapper shape-wrapper d-xl-block d-none">
+                    <div class="shape-image shape-image-1">
+                        <img src="assets/images/shapes/shape-03.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-2">
+                        <img src="assets/images/shapes/shape-15.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-3">
+                        <img src="assets/images/shapes/shape-13-05.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-4">
+                        <img src="assets/images/shapes/shape-05-01.png" alt="Shape Thumb" />
+                    </div>
+                </div> -->
+            </div>
+        </div>
+        <!-- Gallery (end) -->
 
 
         <!-- body will go here (end) -->
